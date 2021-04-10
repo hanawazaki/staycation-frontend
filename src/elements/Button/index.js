bb29de7b05846adce793e8591a2a1d1a530ca78f
hanawazaki <hanawazaki002@gmail.com>
2021-04-10 20:19:52 +0700

    finish header component

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -38,7 +38,7 @@ export default function Button(props) {
           className={className.join(" ")}
           style={props.style}
           target={props.target === "_blank" ? "_blank" : undefined}
-          rel={props.target === "_blank" ? "nooperne noreferrer" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
         >
           {props.children}
         </a>
@@ -80,6 +80,5 @@ Button.propTypes = {
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isBlock: propTypes.bool,
-  isExternal: propTypes.bool,
   hasShadow: propTypes.bool,
 };
