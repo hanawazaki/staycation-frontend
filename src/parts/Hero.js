6832a6bab9ac4b953581259a0825a1cadbeb1c87
hanawazaki <hanawazaki002@gmail.com>
2021-04-12 19:42:27 +0700

    finish hero section

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -8,6 +8,8 @@ import iconTreasure from "assets/images/icons/icon-treasure.svg";
 
 import Button from "elements/Button";
 
+import formatNumber from "utils/formatNumber";
+
 function showMostPicked(props) {
   window.scrollTo({
     top: props.refMostPicked.current.offsetTop - 30,
@@ -24,7 +26,10 @@ export default function Hero(props) {
             Forget Busy Work, <br />
             Start Next Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p
+            className="mb-4 font-weight-light text-gray-500 w-75"
+            style={{ lineHeight: "170%" }}
+          >
             We provide what you need to enjoy your holiday with family. Time to
             make another memorable moments.
           </p>
@@ -37,7 +42,7 @@ export default function Hero(props) {
             Show Me Now
           </Button>
 
-          <div className="row mt-5">
+          <div className="row" style={{ marginTop: 80 }}>
             <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 width="36"
@@ -46,7 +51,7 @@ export default function Hero(props) {
                 alt={`${props.data.travelers} Travelers`}
               />
               <h6 className="mt-3">
-                {props.data.travelers}{" "}
+                {formatNumber(props.data.travelers)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   travelers
                 </span>
@@ -60,7 +65,7 @@ export default function Hero(props) {
                 alt={`${props.data.treasures} treasures`}
               />
               <h6 className="mt-3">
-                {props.data.treasures}{" "}
+                {formatNumber(props.data.treasures)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   treasure
                 </span>
@@ -74,7 +79,7 @@ export default function Hero(props) {
                 alt={`${props.data.cities} Cities`}
               />
               <h6 className="mt-3">
-                {props.data.cities}{" "}
+                {formatNumber(props.data.cities)}{" "}
                 <span className="text-gray-500 font-weight-light">cities</span>
               </h6>
             </div>
